5275de9d5fb8d420b7c44290705b77c189118324
Steve Astels <sastels@gmail.com>
2021-12-05 21:42:47 -0500

    part 2

--- day01/lib/day01.ex
+++ day01/lib/day01.ex
@@ -1,25 +1,43 @@
+# useful:
 # iex -S mix
 # recompile()
 
 defmodule Day01 do
   def split_data(data) do
-    readings = String.split(data)
-    Enum.map(readings, fn s -> Integer.parse(s) |> elem(0) end)
+    String.split(data)
+    |> Enum.map(fn s -> Integer.parse(s) |> elem(0) end)
+  end
+
+  def sum_three(list1) do
+    [_ | list2] = list1
+    [_ | list3] = list2
+
+    Enum.zip([list1, list2, list3])
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.sum(&1))
+  end
+
+  def num_increases(list1) do
+    [_ | list2] = list1
+
+    Enum.zip(list1, list2)
+    |> Enum.filter(&(elem(&1, 1) > elem(&1, 0)))
+    |> length()
   end
 
   def part_1(data) do
-    [first | list2] = data
-    list1 = List.delete_at(data, length(data) - 1)
+    data |> num_increases
+  end
 
-    Enum.zip(list1, list2)
-    |> Enum.map(&(elem(&1, 1) > elem(&1, 0)))
-    |> Enum.filter(& &1)
-    |> length()
+  def part_2(data) do
+    data |> sum_three |> num_increases
   end
 
   def main do
     {:ok, contents} = File.read("data/day01.txt")
     data = split_data(contents)
+
     IO.inspect(part_1(data), label: "part 1")
+    IO.inspect(part_2(data), label: "part 2")
   end
 end
--- day01/test/day01_test.exs
+++ day01/test/day01_test.exs
@@ -12,8 +12,18 @@ defmodule Day01Test do
     assert Day01.split_data(data) == [199, 200, 208, 210]
   end
 
+  test "sum_3 works on example data" do
+    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert Day01.sum_three(data) == [607, 618, 618, 617, 647, 716, 769, 792]
+  end
+
   test "part_1 works on example data" do
     data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
     assert Day01.part_1(data) == 7
+  end
+
+  test("part_2 works on example data") do
+    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert Day01.part_2(data) == 5
   end
 end
