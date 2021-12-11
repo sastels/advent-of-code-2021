7bc2950fa3a72ed4746154e9bca911dbdea4dc80
Steve Astels <sastels@gmail.com>
2021-12-10 20:05:26 -0500

    part 1

--- aoc21/lib/day03.ex
+++ aoc21/lib/day03.ex
@@ -3,12 +3,35 @@
 # recompile()
 
 defmodule Day03 do
-  def parse_bits(s) do
-    s
+  def transpose(rows), do: rows |> List.zip() |> Enum.map(&Tuple.to_list/1)
+
+  def parse_bits(s),
+    do: s |> String.graphemes() |> Enum.map(fn c -> c |> Integer.parse() |> elem(0) end)
+
+  def list_to_num(list) do
+    list
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.join("")
+    |> Integer.parse(2)
+    |> elem(0)
   end
 
   def part_1(data) do
-    data
+    bits =
+      data
+      |> Enum.map(&parse_bits/1)
+      |> transpose()
+      |> Enum.map(fn x ->
+        if Enum.sum(x) > length(x) / 2 do
+          1
+        else
+          0
+        end
+      end)
+
+    gamma = bits |> list_to_num()
+    epsilon = bits |> Enum.map(fn x -> 1 - x end) |> list_to_num()
+    gamma * epsilon
   end
 
   def part_2(data) do
@@ -17,8 +40,7 @@ defmodule Day03 do
 
   def main do
     {:ok, contents} = File.read("data/day03.txt")
-
-    data = contents |> String.split("\n", trim: true) |> Enum.map(&parse_bits/1)
+    data = contents |> String.split("\n", trim: true)
 
     IO.inspect(part_1(data), label: "part 1")
     IO.inspect(part_2(data), label: "part 2")
--- aoc21/test/day03_test.exs
+++ aoc21/test/day03_test.exs
@@ -1,3 +1,39 @@
 defmodule Day03Test do
   use ExUnit.Case
+
+  setup do
+    {:ok,
+     data: [
+       "00100",
+       "11110",
+       "10110",
+       "10111",
+       "10101",
+       "01111",
+       "00111",
+       "11100",
+       "10000",
+       "11001",
+       "00010",
+       "01010"
+     ]}
+  end
+
+  test "parse_bits" do
+    assert "00101" |> Day03.parse_bits() == [0, 0, 1, 0, 1]
+  end
+
+  test "transpose" do
+    assert [[0, 1], [0, 0], [1, 1]] |> Day03.transpose() == [[0, 0, 1], [1, 0, 1]]
+  end
+
+  test "list_to_num" do
+    assert [1, 0, 1, 1] |> Day03.list_to_num() == 11
+    assert [0] |> Day03.list_to_num() == 0
+    assert [0, 0, 1] |> Day03.list_to_num() == 1
+  end
+
+  test "part 1", %{data: data} do
+    assert data |> Day03.part_1() == 198
+  end
 end
