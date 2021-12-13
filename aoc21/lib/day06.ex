ce60cf81a76d520f014cfcbccffd8973f43024a5
Steve Astels <sastels@gmail.com>
2021-12-13 18:57:54 -0500

    part1

--- aoc21/lib/day06.ex
+++ aoc21/lib/day06.ex
@@ -1,9 +1,21 @@
 defmodule Day06 do
-  # @spec parse_data(String.t()) :: list(line_t)
+  @spec step_time(list(number)) :: list(number)
+  def step_time(fish) do
+    num_new_fish = fish |> Enum.filter(fn x -> x == 0 end) |> Enum.count()
+
+    fish =
+      fish
+      |> Enum.map(fn x -> if x > 0, do: x - 1, else: 6 end)
+
+    fish ++ List.duplicate(8, num_new_fish)
+  end
 
   def part_1(contents) do
-    contents
-    |> String.split("\n", trim: true)
+    fish = contents |> String.split(~r/\s*,\a*/, trim: true) |> Enum.map(&String.to_integer/1)
+
+    1..80
+    |> Enum.reduce(fish, fn _, state -> step_time(state) end)
+    |> Enum.count()
   end
 
   def part_2(contents) do
@@ -13,7 +25,11 @@ defmodule Day06 do
 
   def main do
     {:ok, contents} = File.read("data/day06.txt")
-    IO.inspect(part_1(contents), label: "part 1")
-    IO.inspect(part_2(contents), label: "part 2")
+
+    IO.inspect(contents |> String.split("\n", trim: true) |> Enum.at(0) |> part_1(),
+      label: "part 1"
+    )
+
+    # IO.inspect(part_2(contents), label: "part 2")
   end
 end
--- aoc21/test/day06_test.exs
+++ aoc21/test/day06_test.exs
@@ -3,19 +3,19 @@ defmodule Day06Test do
   import Day06
 
   setup do
-    {:ok,
-     contents: """
-     3,4,3,1,2
-     """}
+    {:ok, contents: "3,4,3,1,2"}
   end
 
-  @tag :skip
+  test "step_time", %{contents: contents} do
+    assert [0, 1, 2, 0, 3] |> step_time() == [6, 0, 1, 6, 2, 8, 8]
+  end
+
   test "part 1", %{contents: contents} do
-    assert contents |> Day06.part_1() == 5934
+    assert contents |> part_1() == 5934
   end
 
   @tag :skip
   test "part 2", %{contents: contents} do
-    assert contents |> Day06.part_2() == 0
+    assert contents |> part_2() == 0
   end
 end
