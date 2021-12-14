65a148d58c8cd444bdab8507f75cc58d1080a9e6
Steve Astels <sastels@gmail.com>
2021-12-13 20:54:15 -0500

    use map instead of list

--- aoc21/lib/day06.ex
+++ aoc21/lib/day06.ex
@@ -10,12 +10,44 @@ defmodule Day06 do
     fish ++ List.duplicate(8, num_new_fish)
   end
 
-  def part_1(contents) do
-    fish = contents |> String.split(~r/\s*,\a*/, trim: true) |> Enum.map(&String.to_integer/1)
+  def part_1_old(contents) do
+    fish = contents |> String.split(~r/\s*,\s*/, trim: true) |> Enum.map(&String.to_integer/1)
 
     1..80
     |> Enum.reduce(fish, fn _, state -> step_time(state) end)
     |> Enum.count()
+  end
+
+  def part_1(contents) do
+    fish = parse_fish_map(contents)
+
+    1..80
+    |> Enum.reduce(fish, fn _, state -> step_fish_map(state) end)
+    |> Enum.map(fn {_, v} -> v end)
+    |> Enum.sum()
+  end
+
+  def parse_fish_map(s) do
+    s
+    |> String.split("\n", trim: true)
+    |> Enum.at(0)
+    |> String.split(~r/\s*,\s*/, trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(%{0 => 0}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+  end
+
+  def step_fish_map(fish) do
+    %{
+      0 => Map.get(fish, 1, 0),
+      1 => Map.get(fish, 2, 0),
+      2 => Map.get(fish, 3, 0),
+      3 => Map.get(fish, 4, 0),
+      4 => Map.get(fish, 5, 0),
+      5 => Map.get(fish, 6, 0),
+      6 => Map.get(fish, 7, 0) + Map.get(fish, 0, 0),
+      7 => Map.get(fish, 8, 0),
+      8 => Map.get(fish, 0, 0)
+    }
   end
 
   def part_2(contents) do
--- aoc21/test/day06_test.exs
+++ aoc21/test/day06_test.exs
@@ -6,8 +6,26 @@ defmodule Day06Test do
     {:ok, contents: "3,4,3,1,2"}
   end
 
-  test "step_time", %{contents: contents} do
+  test "step_time" do
     assert [0, 1, 2, 0, 3] |> step_time() == [6, 0, 1, 6, 2, 8, 8]
+  end
+
+  test "parse_fish_map" do
+    assert "0, 1, 2, 0, 3\n" |> parse_fish_map() == %{0 => 2, 1 => 1, 2 => 1, 3 => 1}
+  end
+
+  test "step_fish_map" do
+    assert %{0 => 2, 3 => 2, 2 => 10} |> step_fish_map() == %{
+             0 => 0,
+             1 => 10,
+             2 => 2,
+             3 => 0,
+             4 => 0,
+             5 => 0,
+             6 => 2,
+             7 => 0,
+             8 => 2
+           }
   end
 
   test "part 1", %{contents: contents} do
