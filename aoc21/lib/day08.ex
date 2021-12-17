a5faec0cb22e5bede5d2bb095d618d1979f0c70c
Steve Astels <sastels@gmail.com>
2021-12-17 15:10:09 -0500

    part 1

--- aoc21/lib/day08.ex
+++ aoc21/lib/day08.ex
@@ -1,6 +1,21 @@
+# counts
+
 defmodule Day08 do
+  def parse_line(s) do
+    [s1, s2] = String.split(s, ~r/\s*\|\s*/, trim: true)
+    patterns = s1 |> String.split(~r/\s+/, trim: true) |> Enum.map(&String.graphemes/1)
+    values = s2 |> String.split(~r/\s+/, trim: true) |> Enum.map(&String.graphemes/1)
+    {patterns, values}
+  end
+
   def part_1(contents) do
     contents
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.concat()
+    |> Enum.filter(fn x -> Enum.member?([2, 4, 3, 7], length(x)) end)
+    |> Enum.count()
   end
 
   def part_2(contents) do
@@ -8,7 +23,7 @@ defmodule Day08 do
   end
 
   def main do
-    {:ok, contents} = File.read("data/day07.txt")
+    {:ok, contents} = File.read("data/day08.txt")
     IO.inspect(contents |> part_1(), label: "part 1")
     # IO.inspect(contents |> part_2(), label: "part 2")
   end
--- aoc21/test/day08_test.exs
+++ aoc21/test/day08_test.exs
@@ -3,12 +3,31 @@ defmodule Day08Test do
   import Day08
 
   setup do
-    {:ok, contents: "hi there"}
+    {:ok,
+     contents: """
+     be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+     edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+     fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+     fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+     aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+     fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+     dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+     bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+     egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+     gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+     """}
   end
 
-  @tag :skip
+  test "parse_line" do
+    assert parse_line("be cfbegad cbdgef | fdgacbe cefdb ") ==
+             {
+               [["b", "e"], ["c", "f", "b", "e", "g", "a", "d"], ["c", "b", "d", "g", "e", "f"]],
+               [["f", "d", "g", "a", "c", "b", "e"], ["c", "e", "f", "d", "b"]]
+             }
+  end
+
   test "part 1", %{contents: contents} do
-    assert contents |> part_1() == nil
+    assert contents |> part_1() == 26
   end
 
   @tag :skip
