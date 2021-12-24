b431353dd0a32e7b2a4541e870113c326aecbc23
Steve Astels <sastels@gmail.com>
2021-12-24 13:48:25 -0500

    init

---
+++ aoc21/lib/day13.ex
@@ -0,0 +1,15 @@
+defmodule Day13 do
+  def part_1(contents) do
+    contents
+  end
+
+  def part_2(contents) do
+    contents
+  end
+
+  def main do
+    {:ok, contents} = File.read("data/day00.txt")
+    IO.inspect(contents |> part_1(), label: "part 1")
+    # IO.inspect(contents |> part_2(), label: "part 2")
+  end
+end
---
+++ aoc21/test/day13_test.exs
@@ -0,0 +1,21 @@
+defmodule Day13Test do
+  use ExUnit.Case
+  import Day13
+
+  setup do
+    {:ok,
+     contents: """
+     hi there
+     """}
+  end
+
+  # @tag :skip
+  # test "part 1", %{contents: contents} do
+  #   assert contents |> part_1() == nil
+  # end
+
+  # @tag :skip
+  # test "part 2", %{contents: contents} do
+  #   assert contents |> part_2() == nil
+  # end
+end
