0375db995d552ff3af9d6ea5c659b8df1711560c
Steve Astels <sastels@gmail.com>
2021-12-26 21:03:57 -0500

    part 1

--- aoc21/lib/day14.ex
+++ aoc21/lib/day14.ex
@@ -1,10 +1,71 @@
 defmodule Day14 do
+  def frequencies(list),
+    do: list |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+
+  def parse_template(data) do
+    data
+    |> String.split("\n", trim: true)
+    |> Enum.reject(&String.contains?(&1, "->"))
+    |> List.first()
+  end
+
+  def parse_rule(s) do
+    s
+    |> String.replace(" -> ", "")
+    |> String.graphemes()
+    |> then(fn [a, b, c] -> %{(a <> b) => a <> c <> b} end)
+  end
+
+  def parse_rules(data) do
+    data
+    |> String.split("\n", trim: true)
+    |> Enum.filter(&String.contains?(&1, "->"))
+    |> Enum.map(&parse_rule/1)
+    |> Enum.reduce(%{}, &Map.merge/2)
+  end
+
+  def apply_rules_to_pair(pair, rules), do: Map.get(rules, pair, pair)
+
+  def apply_rules(s, rules) do
+    last_char = String.graphemes(s) |> List.last()
+
+    (s
+     |> String.graphemes()
+     |> Enum.chunk_every(2, 1)
+     |> Enum.map(&Enum.join/1)
+     |> Enum.map(&apply_rules_to_pair(&1, rules))
+     |> Enum.map(fn s -> s |> String.graphemes() |> Enum.drop(-1) |> Enum.join() end)
+     |> Enum.join()) <> last_char
+  end
+
+  def apply_rules(s, rules, n) do
+    Range.new(1, n)
+    |> Enum.reduce(s, fn n, acc ->
+      IO.inspect(n)
+      apply_rules(acc, rules)
+    end)
+  end
+
   def part_1(contents) do
-    contents
+    template = parse_template(contents)
+    rules = parse_rules(contents)
+
+    letter_frequencies = apply_rules(template, rules, 10) |> String.graphemes() |> frequencies()
+
+    max_freq = letter_frequencies |> Enum.map(&elem(&1, 1)) |> Enum.max()
+    min_freq = letter_frequencies |> Enum.map(&elem(&1, 1)) |> Enum.min()
+    max_freq - min_freq
   end
 
   def part_2(contents) do
-    contents
+    template = parse_template(contents)
+    rules = parse_rules(contents)
+
+    letter_frequencies = apply_rules(template, rules, 40) |> String.graphemes() |> frequencies()
+
+    max_freq = letter_frequencies |> Enum.map(&elem(&1, 1)) |> Enum.max()
+    min_freq = letter_frequencies |> Enum.map(&elem(&1, 1)) |> Enum.min()
+    max_freq - min_freq
   end
 
   def main do
--- aoc21/test/day14_test.exs
+++ aoc21/test/day14_test.exs
@@ -26,13 +26,40 @@ defmodule Day14Test do
      """}
   end
 
-  @tag :skip
-  test "part 1", %{contents: contents} do
-    assert contents |> part_1() == nil
+  test "parse_template" do
+    assert "aba\n\na->bb" |> parse_template() == "aba"
   end
 
-  @tag :skip
+  test "parse_rule" do
+    assert "ch -> b" |> parse_rule() == %{"ch" => "cbh"}
+  end
+
+  test "parse_rules" do
+    assert "aba\n\nab -> c\nqb -> H" |> parse_rules() ==
+             %{"ab" => "acb", "qb" => "qHb"}
+  end
+
+  test "apply_rules_to_pair", %{contents: contents} do
+    rules = parse_rules(contents)
+    assert "CH" |> apply_rules_to_pair(rules) == "CBH"
+    assert "ab" |> apply_rules_to_pair(rules) == "ab"
+  end
+
+  test "apply_rules", %{contents: contents} do
+    rules = parse_rules(contents)
+    assert "NNCB" |> apply_rules(rules) == "NCNBCHB"
+  end
+
+  test "apply_rules 2", %{contents: contents} do
+    rules = parse_rules(contents)
+    assert "NNCB" |> apply_rules(rules, 2) == "NBCCNBBBCBHCB"
+  end
+
+  test "part 1", %{contents: contents} do
+    assert contents |> part_1() == 1588
+  end
+
   test "part 2", %{contents: contents} do
-    assert contents |> part_2() == nil
+    assert contents |> part_2() == 2_188_189_693_529
   end
 end
