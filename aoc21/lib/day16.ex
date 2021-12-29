78bbe4d5653a758e8d6fd4bccee80059b97f2e27
Steve Astels <sastels@gmail.com>
2021-12-28 19:48:56 -0500

    init

---
+++ aoc21/lib/day16.ex
@@ -0,0 +1,15 @@
+defmodule Day16 do
+  def part_1(contents) do
+    contents
+  end
+
+  def part_2(contents) do
+    contents
+  end
+
+  def main do
+    {:ok, contents} = File.read("data/day16.txt")
+    IO.inspect(contents |> part_1(), label: "part 1")
+    # IO.inspect(contents |> part_2(), label: "part 2")
+  end
+end
