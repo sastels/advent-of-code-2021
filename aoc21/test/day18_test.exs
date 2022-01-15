d98691239b3b2425090064879e7d46a358d5f548
Steve Astels <sastels@gmail.com>
2022-01-15 18:30:24 -0500

    init

---
+++ aoc21/test/day18_test.exs
@@ -0,0 +1,21 @@
+defmodule Day18Test do
+  use ExUnit.Case
+  import Day18
+
+  setup do
+    {:ok,
+     contents: """
+     hi there
+     """}
+  end
+
+  # @tag :skip
+  # test "part 1", %{contents: contents} do
+  #   assert contents |> part_1() == nil
+  # end
+
+  # @tag :skip
+  # test "part 2", %{contents: contents} do
+  #   assert contents |> part_2() == nil
+  # end
+end
