d7d8e4f68448f954ce05a0bccb9a1ea9fdeca0e7
Steve Astels <sastels@gmail.com>
2022-01-15 20:10:49 -0500

    explody

--- aoc21/lib/day18.ex
+++ aoc21/lib/day18.ex
@@ -1,4 +1,117 @@
 defmodule Day18 do
+  @type snail :: String.t()
+
+  @spec add(snail, snail) :: snail
+  def add(sn0, sn1), do: "[#{sn0},#{sn1}]"
+
+  @spec count_depth(String.t()) :: integer()
+  def count_depth(s) do
+    s
+    |> String.graphemes()
+    |> Enum.map(fn c ->
+      case c do
+        "[" -> 1
+        "]" -> -1
+        _ -> 0
+      end
+    end)
+    |> Enum.sum()
+  end
+
+  @spec first_simple_snail(snail()) :: [String.t()]
+  def first_simple_snail(sn) do
+    Regex.split(~r{\[\d+\,\d+\]}, sn, parts: 2, include_captures: true)
+  end
+
+  @spec first_depth_n(String.t(), integer) :: [String.t()]
+  def first_depth_n(s, n) do
+    split = first_simple_snail(s)
+
+    if length(split) == 1 do
+      [s]
+    else
+      [pre, sn, post] = split
+
+      if count_depth(pre) == n do
+        split
+      else
+        recurse = first_depth_n(post, n - count_depth(pre))
+
+        if length(recurse) == 1 do
+          [s]
+        else
+          [pre <> sn <> recurse[0], recurse[1], recurse[2]]
+        end
+      end
+    end
+  end
+
+  @spec first_number(String.t()) :: [String.t() | integer]
+  def first_number(s) do
+    n = Regex.scan(~r/\d+/, s) |> List.flatten() |> List.first()
+
+    if n == nil do
+      [s]
+    else
+      [pre, post] = String.split(s, n, parts: 2)
+      [pre, String.to_integer(n), post]
+    end
+  end
+
+  @spec last_number(String.t()) :: [String.t() | integer]
+  def last_number(s) do
+    results = s |> String.reverse() |> first_number
+
+    if length(results) == 1 do
+      [s]
+    else
+      [pre, n, post] = results
+
+      [
+        String.reverse(post),
+        n |> Integer.to_string() |> String.reverse() |> String.to_integer(),
+        String.reverse(pre)
+      ]
+    end
+  end
+
+  @spec explode([String.t()]) :: snail
+  def explode(parts) do
+    [pre, sn, post] = parts
+    [_, n0, _] = first_number(sn)
+    [_, n1, _] = last_number(sn)
+    pre_last = last_number(pre)
+    post_first = first_number(post)
+
+    new_pre =
+      if length(pre_last) == 1 do
+        pre
+      else
+        [pre_pre, pre_last_n, pre_post] = pre_last
+        pre_pre <> "#{pre_last_n + n0}" <> pre_post
+      end
+
+    new_post =
+      if length(post_first) == 1 do
+        post
+      else
+        [post_pre, post_first_n, post_post] = post_first
+        post_pre <> "#{post_first_n + n1}" <> post_post
+      end
+
+    new_pre <> "0" <> new_post
+  end
+
+  def try_explode(sn) do
+    depth_4 = first_depth_n(sn, 4)
+
+    if length(depth_4) == 1 do
+      {sn, false}
+    else
+      {explode(depth_4), true}
+    end
+  end
+
   def part_1(contents) do
     contents
   end
--- aoc21/test/day18_test.exs
+++ aoc21/test/day18_test.exs
@@ -9,6 +9,55 @@ defmodule Day18Test do
      """}
   end
 
+  test "add" do
+    assert add("[1,2]", "[3,[4,5]]") == "[[1,2],[3,[4,5]]]"
+  end
+
+  test "count_depth" do
+    assert count_depth("[1,2]") == 0
+    assert count_depth("[5,[5,2]") == 1
+    assert count_depth("d,],[1,2]]") == -2
+  end
+
+  test "first_simple_snail" do
+    assert "]" |> first_simple_snail == ["]"]
+    assert "[1,2]" |> first_simple_snail == ["", "[1,2]", ""]
+    assert "[1,[2,3]]" |> first_simple_snail == ["[1,", "[2,3]", "]"]
+  end
+
+  test "first_depth_n" do
+    assert "[[[[[9,8],1],2],3],4]" |> first_depth_n(4) == ["[[[[", "[9,8]", ",1],2],3],4]"]
+    assert "[7,[6,[5,[4,[3,2]]]]]" |> first_depth_n(4) == ["[7,[6,[5,[4,", "[3,2]", "]]]]"]
+    assert "[[6,[5,[4,[31,222]]]],1]" |> first_depth_n(4) == ["[[6,[5,[4,", "[31,222]", "]]],1]"]
+    assert "[1,2]" |> first_depth_n(4) == ["[1,2]"]
+    assert "[[1,2],[[3,3],[4,5]]]" |> first_depth_n(4) == ["[[1,2],[[3,3],[4,5]]]"]
+  end
+
+  test "first_number" do
+    assert "hi" |> first_number == ["hi"]
+    assert "[[61,[5,[4,[" |> first_number == ["[[", 61, ",[5,[4,["]
+  end
+
+  test "last_number" do
+    assert "hi" |> last_number == ["hi"]
+    assert "[[61,[5,[41,[" |> last_number == ["[[61,[5,[", 41, ",["]
+  end
+
+  test "explode" do
+    assert ["[[[[", "[9,8]", ",1],2],3],4]"] |> explode == "[[[[0,9],2],3],4]"
+    assert ["[7,[6,[5,[4,", "[3,2]", "]]]]"] |> explode == "[7,[6,[5,[7,0]]]]"
+    assert ["[[6,[5,[4,", "[3,2]", "]]],1]"] |> explode == "[[6,[5,[7,0]]],3]"
+  end
+
+  test "try_explode" do
+    assert "[1,2]" |> try_explode == {"[1,2]", false}
+    assert "[[1,2],[[3,3],[4,5]]]" |> try_explode == {"[[1,2],[[3,3],[4,5]]]", false}
+    assert "[[[[[9,8],1],2],3],4]" |> try_explode == {"[[[[0,9],2],3],4]", true}
+
+    assert "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]" |> try_explode ==
+             {"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", true}
+  end
+
   # @tag :skip
   # test "part 1", %{contents: contents} do
   #   assert contents |> part_1() == nil
